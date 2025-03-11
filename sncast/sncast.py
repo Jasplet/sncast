07b7fa9fc817621f43252551890f9f3af369ab10
Joseph Asplet <joseph.asplet@earth.ox.ac.uk>
2025-03-11 12:13:37 +0000

    sncast.py now in dir sncast

--- sncast/sncast.py
+++ sncast/sncast.py
@@ -36,6 +36,13 @@
 #    web:         www.dias.ie/martin  www.insn.ie
 #
 # --------------------------------------------------------------------
+# This version of SNCAST has been refactored and extended
+# to add various additional functionality such as:
+#   - Implementation of BGS Local magnitude
+#   - Implementation of GMPE based method
+#   - Functionality to calculate of a depth cross-section
+#   - Outputs model as xarray DataArray, for easier plotting
+#   - Added support for seismic arrays
 
 import numpy as np
 import pandas as pd
@@ -86,70 +93,6 @@ GMPES = {'RE19': {'PGV': {'c1': -4.4578, 'c2': 1.6540,
         }
 
 
-def convert_mw_to_ml(mw, region='UK'):
-    '''
-    Converts Moment magntiude to local magnitude
-    using an empirical scaling relationship
-
-    Default region is the UK.
-    For small M<3 event we use the conversion developed by Butcher et al., (2020)
-    https://doi.org/10.1785/0120190032 using data from
-    New Ollerton and is applicable for small magntiude events.
-
-    For Mw > 3 the Ottemöller and Sargeant (2013) https://doi.org/10.1785/0120130085
-    conversion is used instead.
-
-    Parameters:
-    ----------
-    mw : numpy.Array
-        array of moment magnitudes to convert
-    region : str
-        Region's scaling relationship to use. Defaults to UK
-    '''
-
-    if region == 'UK':
-        butcher_uk_small_mw = np.vectorize(lambda x: (x - 0.75) / 0.69)
-        ottermoller_uk = np.vectorize(lambda x: (x - 0.23) / 0.85)
-
-        ml = np.where(mw <= 3, butcher_uk_small_mw(mw), ottermoller_uk(mw))
-    else:
-        raise ValueError(f'Unsupported region {region}')
-
-    return ml
-
-
-def convert_ml_to_mw(ml, region='UK'):
-    '''
-    Converts Local magnitude to moment magnitude
-    using an empirical scaling relationship
-
-    Default region is the UK.
-    For small M<3 event we use the conversion developed by Butcher et al., (2020)
-    https://doi.org/10.1785/0120190032 using data from
-    New Ollerton and is applicable for small magntiude events.
-
-    For Mw > 3 the Ottemöller and Sargeant (2013) https://doi.org/10.1785/0120130085
-    conversion is used instead.
-
-    Parameters:
-    ----------
-    mw : numpy.Array
-        array of moment magnitudes to convert
-    region : str
-        Region's scaling relationship to use. Defaults to UK
-    '''
-
-    if region == 'UK':
-        butcher_uk_small_mw = np.vectorize(lambda x: 0.75*x + 0.69)
-        ottermoller_uk = np.vectorize(lambda x: 0.85*x + 0.23)
-
-        ml = np.where(ml <= 3, butcher_uk_small_mw(ml), ottermoller_uk(ml))
-    else:
-        raise ValueError(f'Unsupported region {region}')
-
-    return mw
-
-
 def calc_pgv(mw, epic_dist, author, model_type='PGV'):
 
     coeffs = GMPES[author][model_type]
@@ -189,7 +132,8 @@ def _re19_f_terms(dist, r0=10, r1=50, r2=100):
 
     return f0, f1, f2
 
-def calc_ampl(local_mag, hypo_dist, region):
+
+def calc_ampl_from_magnitude(local_mag, hypo_dist, region):
 
     #   region specific ML = log(ampl) + a*log(hypo-dist) + b*hypo_dist + c
     if region == 'UK':
@@ -277,7 +221,7 @@ def minML(stations_in, lon0=-12, lon1=-4, lat0=50.5, lat1=56.6, dlon=0.33,
                 m = mag_min - mag_delta
                 while ampl < snr*noise[j]: 
                     m = m + mag_delta
-                    ampl = calc_ampl(m, hypo_dist, region)
+                    ampl = calc_ampl_from_magnitude(m, hypo_dist, region)
                 mag.append(m)
             # sort magnitudes in ascending order
             mag = sorted(mag)
