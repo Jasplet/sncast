81a444fbc1c06e87fdf98c39575c8c7f21da5ef9
Joseph Asplet <joseph.asplet@earth.ox.ac.uk>
2025-10-09 16:33:50 +0100

    changed theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,25 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'SN-CAST'
-copyright = '2025, Joseph Asplet, 2019, Martin Molhoff'
-author = 'Joseph Asplet, Martin Molhoff'
-release = '0.1'
+project = "SN-CAST"
+copyright = "2025, Joseph Asplet, 2019, Martin Molhoff"
+author = "Joseph Asplet, Martin Molhoff"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc']
+extensions = ["sphinx.ext.duration", "sphinx.ext.doctest", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
